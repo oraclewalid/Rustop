63e549abebb5c245551fd98796d7d2ce2c7fe73e
walid chergui <walidchergui@gmail.com>
2018-10-21 21:11:13 +0200

    add ui

--- src/main.rs
+++ src/main.rs
@@ -1,63 +1,20 @@
-extern crate rand;
-extern crate sysinfo;
+extern crate failure;
+extern crate termion;
+extern crate tui;
+mod ui;
+mod event_handler;
 
-use rand::Rng;
-use std::cmp::Ordering;
-use std::io;
-use sysinfo::{NetworkExt, System, SystemExt};
-fn main() {
-	// We display the disks:
-	
-	let mut sys = System::new();
-	println!("=> disk list:");
-	for disk in sys.get_disks() {
-	    println!("{:?}", disk);
-	}
 
-	// Network data:
-	println!("input data : {} B", sys.get_network().get_income());
-	println!("output data: {} B", sys.get_network().get_outcome());
+use std::sync::mpsc;
+use event_handler::Event;
+use std::thread;
 
-	// Components temperature:
-	for component in sys.get_components_list() {
-	    println!("{:?}", component);
-	}
+fn main() -> Result<(), failure::Error> {
 
-	// Memory information:
-	println!("total memory: {} kB", sys.get_total_memory());
-	println!("used memory : {} kB", sys.get_used_memory());
-	println!("total swap  : {} kB", sys.get_total_swap());
-	println!("used swap   : {} kB", sys.get_used_swap());
 
-	// Number of processes
-	println!("NB processes: {}", sys.get_processor_list().len());
-
-	for process in sys.get_processor_list() {
-	    println!("{:?}", process);
-	}
-
-	// To refresh all system information:
-	sys.refresh_all();
-}
-
-fn old() {
-
-  
-    loop {
-    	 let mut guess = String::new();
-
-	    io::stdin().read_line(&mut guess)
-	        .expect("Failed to read line");
-
-    	let guess: u32 = match guess.trim().parse() {
-	    	Ok(num) => num,
-	    	Err(_) => continue,
-		};
-		let random = rand::thread_rng().gen_range(1, 101);
-	    match random.cmp(&guess) {
-	    	Ordering::Less => println!("small"),
-	    	Ordering::Greater => println!("big"),
-	    	Ordering::Equal=> println!("Equal"),
-	    }
-    }
+	let (tx, rx) = mpsc::channel();
+	thread::spawn(move || {
+		let x = tx.send(Event::CpuEvent())
+	});
+	ui::launch_ui()
 }
