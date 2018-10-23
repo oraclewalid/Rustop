24bd5c1dedfd29c1cfa4687641dbaa756b3b9894
walid chergui <walidchergui@gmail.com>
2018-10-24 00:00:51 +0200

    now :hankey: compile

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,24 @@
 extern crate systemstat;
 
-use systemstat::{System, Platform};
+use metrics_collector::MetricsCollector;
 use std::sync::mpsc;
+use std::thread;
+use std::time::Duration;
+use systemstat::{Platform, System};
 
 mod metrics_collector;
 mod event;
+
 fn main() {
 	let platform= System::new();
 	let (tx, rx) = mpsc::channel();
-	let metrics = metrics_collector::MetricsCollector::new(tx, platform);
 
-	metrics.run()
+
+	let _ = thread::spawn(move || {
+		let metrics = MetricsCollector{channel : tx, sys: platform};
+		metrics.run()
+	});
+
+	thread::sleep(Duration::from_secs(10));
+
 }
--- src/metrics_collector.rs
+++ src/metrics_collector.rs
@@ -1,12 +1,12 @@
 use std::sync::mpsc::Sender;
 use std::thread;
 use std::time::Duration;
-use systemstat::{System, Platform};
 use super::event::Event;
+use systemstat::{Platform, System};
 
 pub struct MetricsCollector {
-    channel: Sender<Event>,
-    sys: System,
+    pub channel: Sender<Event>,
+    pub sys: System,
 }
 
 impl MetricsCollector {
@@ -15,19 +15,15 @@ impl MetricsCollector {
         return MetricsCollector { channel, sys };
     }
 
-    pub fn run(&self) -> (){
-
-            let tx1 = Sender::clone(&self.channel);
-            thread::spawn(move || {
-                match self.sys.cpu_load_aggregate() {
-                    Ok(cpu)=> {
-                        thread::sleep(Duration::from_secs(1));
-                        let cpu = cpu.done().unwrap();
-                        //let result = tx1.send(Event::CpuEvent(cpu.user * 100.0));
-                        println!("CPU load: {}", cpu.user * 100.0);
-                    },
-                    Err(x) => println!("\nCPU load: error: {}", x)
-                }
-            });
+    pub fn run(&self) -> () {
+        match self.sys.cpu_load_aggregate() {
+            Ok(cpu) => {
+                thread::sleep(Duration::from_secs(1));
+                let cpu = cpu.done().unwrap();
+                let result = self.channel.send(Event::CpuEvent(cpu.user * 100.0));
+                println!("CPU load: {}", cpu.user * 100.0);
+            }
+            Err(x) => println!("CPU load: error: {}", x)
+        }
     }
 }
